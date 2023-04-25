21e12992208ea5bb101d8c59caf1bd0770f2afad
ruhimahendra24 <ruhimahendra24@gmail.com>
2023-04-25 19:47:36 -0400

    ew

--- Machine_Learning_Engineering/app.py
+++ Machine_Learning_Engineering/app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 
 # Load the machine learning model and the CSV data
 model = pickle.load(open("/Users/ruhimahendra/Desktop/Football_Transfermarkt/Machine_Learning_Engineering/model.pkl", "rb"))
-data = pd.read_csv('/Users/ruhimahendra/Desktop/Football_Transfermarkt/Machine_Learning_Engineering/feed_model.csv')
+with open('/Users/ruhimahendra/Desktop/Football_Transfermarkt/Machine_Learning_Engineering/my_list.pkl', 'rb') as data:
+    my_array = pickle.load(data)
+
 
 @app.route('/')
 def index():
@@ -20,14 +22,14 @@ def predict():
     id = request.form['id']
     
     # Get the corresponding row from the data
-    row = data[data['index'] == id]
+    row = my_array[int(id)]
     
     # Get the features from the row
-    features = row.drop(['index'], axis=1)
+    features_array = row.reshape(1,-1)
+    print(features_array.shape)
     
     # Make a prediction using the machine learning model
-    prediction = model.predict(features)
-    
+    prediction = model.predict(features_array)    
     # Return the prediction to the user
     return f'The predicted value for ID {id} is {prediction[0]}.'
 
