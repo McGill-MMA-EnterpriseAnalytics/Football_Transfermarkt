c7a9478406adc643884407aed1158d6e2f8bbfbc
ruhimahendra24 <ruhimahendra24@gmail.com>
2023-04-25 23:09:14 -0400

    change path againnn

--- Machine_Learning_Engineering/app.py
+++ Machine_Learning_Engineering/app.py
@@ -6,8 +6,8 @@ import pickle
 app = Flask(__name__)
 
 # Load the machine learning model and the CSV data
-model = pickle.load(open("/Machine_Learning_Engineering/model.pkl", "rb"))
-with open('/Machine_Learning_Engineering/my_list.pkl', 'rb') as data:
+model = pickle.load(open("model.pkl", "rb"))
+with open('my_list.pkl', 'rb') as data:
     my_array = pickle.load(data)
 
 
